86ed44ecb787b25ab82055f9b73d705242c209cb
Binlogo <binboy@live.com>
2021-03-11 13:15:57 +0800

    exercise: finish clippy1

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -6,18 +6,11 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-use std::f32;
-
 fn main() {
-    let pi = 3.14f32;
-    let radius = 5.00f32;
-
-    let area = pi * f32::powi(radius, 2);
-
-    println!(
-        "The area of a circle with radius {:.2} is {:.5}!",
-        radius, area
-    )
+    let x = 1.2331f64;
+    let y = 1.2332f64;
+    let p: f64 = 10f64.powf(-(6_f64));
+    if (y - x).abs() < p {
+        println!("Success!");
+    }
 }
