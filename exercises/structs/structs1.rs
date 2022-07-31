3dd5a933fb87988e8f9004e1efc3572a77c3b523
棒棒彬 <binboy@live.com>
2022-07-31 12:09:48 +0800

    exercise more: update structs

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -2,12 +2,13 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a hint.
 
-struct ColorClassicStruct<'a> {
-    name: &'a str,
-    hex: &'a str,
+struct ColorClassicStruct {
+    red: u8,
+    green: u8,
+    blue: u8,
 }
 
-struct ColorTupleStruct<'a>(&'a str, &'a str);
+struct ColorTupleStruct(u8, u8, u8);
 
 #[derive(Debug)]
 struct UnitLikeStruct;
@@ -18,10 +19,7 @@ mod tests {
 
     #[test]
     fn classic_c_structs() {
-        let green = ColorClassicStruct {
-            name: "green",
-            hex: "#00FF00",
-        };
+        let green = ColorClassicStruct { red: 0, green: 255, blue: 0 };
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -30,7 +28,7 @@ mod tests {
 
     #[test]
     fn tuple_structs() {
-        let green = ColorTupleStruct("green", "#00FF00");
+        let green = ColorTupleStruct(0, 255, 0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -39,8 +37,8 @@ mod tests {
 
     #[test]
     fn unit_structs() {
-        let unit_struct = UnitStruct;
-        let message = format!("{:?}s are fun!", unit_struct);
+        let unit_like_struct = UnitLikeStruct;
+        let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
     }
